185686784edbc57848243a0f8abcb416d29ad133
Marta Carlos <marta.carlos@outsystems.com>
2024-10-29 12:22:19 +0000

    feat: google-maps cap7 ready
    - upgrades plugin's dependencies to google maps
    - iOS: removes deprecated code && fixes a bug where iOS location permissions weren't being requested

--- plugin/ios/Plugin/CapacitorGoogleMapsPlugin.swift
+++ plugin/ios/Plugin/CapacitorGoogleMapsPlugin.swift
@@ -67,11 +67,13 @@ extension CGRect {
 public class CapacitorGoogleMapsPlugin: CAPPlugin, GMSMapViewDelegate {
     private var maps = [String: Map]()
     private var isInitialized = false
-
+    private var locationManager = CLLocationManager()
+    
+    
     func checkLocationPermission() -> String {
         let locationState: String
 
-        switch CLLocationManager.authorizationStatus() {
+        switch self.locationManager.authorizationStatus {
         case .notDetermined:
             locationState = "prompt"
         case .restricted, .denied:
@@ -682,8 +684,10 @@ public class CapacitorGoogleMapsPlugin: CAPPlugin, GMSMapViewDelegate {
             guard let enabled = call.getBool("enabled") else {
                 throw GoogleMapErrors.invalidArguments("enabled is missing")
             }
+            
+            let locationStatus = checkLocationPermission()
 
-            if enabled && checkLocationPermission() != "granted" {
+            if enabled &&  !(locationStatus == "granted" || locationStatus == "prompt") {
                 throw GoogleMapErrors.permissionsDeniedLocation
             }
 
