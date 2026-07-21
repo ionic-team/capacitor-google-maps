19868e4123f94a8f42ddd967c923dfa318bbf3a6
jcesarmobile <jcesarmobile@gmail.com>
2026-07-21 18:09:10 +0200

    chore!: Align plugin baselines with core (#171)

--- README.md
+++ README.md
@@ -65,8 +65,7 @@ This plugin will use the following project variables (defined in your app's `var
 - `googleMapsKtxVersion`: version of `com.google.maps.android:maps-ktx` (default: `5.2.1`)
 - `googleMapsUtilsKtxVersion`: version of `com.google.maps.android:maps-utils-ktx` (default: `5.2.1`)
 - `kotlinxCoroutinesVersion`: version of `org.jetbrains.kotlinx:kotlinx-coroutines-android` and `org.jetbrains.kotlinx:kotlinx-coroutines-core` (default: `1.10.2`)
-- `androidxCoreKTXVersion`: version of `androidx.core:core-ktx` (default: `1.17.0`)
-- `kotlin_version`: version of `org.jetbrains.kotlin:kotlin-stdlib` (default: `2.2.20`)
+- `androidxCoreVersion`: version of `androidx.core:core` (default: `1.19.0`)
 
 
 ## Usage
--- plugin/Package.swift
+++ plugin/Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorGoogleMaps",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "CapacitorGoogleMaps",
             targets: ["CapacitorGoogleMapsPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5"),
         .package(url: "https://github.com/googlemaps/ios-maps-sdk.git", .upToNextMajor(from:"9.4.0")),
         .package(url: "https://github.com/googlemaps/google-maps-ios-utils.git", .exact("6.1.0"))
     ],
@@ -19,7 +19,6 @@ let package = Package(
             name: "CapacitorGoogleMapsPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "GoogleMaps", package: "ios-maps-sdk"),
                 .product(name: "GoogleMapsUtils", package: "google-maps-ios-utils")
             ],
