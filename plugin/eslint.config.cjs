3f221baa1003752cefaf272fab5ec12eb3982000
Pedro Bilro <pedro.gustavo.bilro@outsystems.com>
2026-07-29 13:58:24 +0100

    chore: fix running lint locally
    Clone of https://github.com/ionic-team/capacitor-barcode-scanner/pull/127

--- plugin/eslint.config.cjs
+++ plugin/eslint.config.cjs
@@ -5,7 +5,7 @@ const prettierConfig = require('eslint-config-prettier');
 
 module.exports = [
   {
-    ignores: ['node_modules/**', 'dist/**', 'build/**', 'unit-tests/**', 'android/**', 'ios/**', 'types/**', 'eslint.config.*'],
+    ignores: ['node_modules/**', 'dist/**', 'build/**', '.build/**', 'unit-tests/**', 'android/**', 'ios/**', 'types/**', 'eslint.config.*'],
   },
   eslintjs.configs.recommended,
   {
